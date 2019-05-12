a822bf602db4f660b22a2cc24caab72d8562fa6b
cufrecillo <cufrecillo@gmail.com>
2019-05-12 11:42:28 +0200

    funciones retroactividad factorial

---
+++ sumatorioRec.py
@@ -0,0 +1,15 @@
+def sumatorio(n):
+    if n > 0:
+        return n + sumatorio(n-1)
+    else:
+        return 0
+    
+print(sumatorio(4))
+
+def factorial(n):
+    if n > 0:
+        return n * factorial(n-1)
+    else:
+        return 1
+    
+print(factorial(4))
